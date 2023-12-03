50167a3fbfba2d9216458a30f93fbf794591724b
Naveen Prakash <naveenprakashjk24@gmail.com>
2023-12-03 20:47:59 +0530

    working user admin customization

--- app/core/admin.py
+++ app/core/admin.py
@@ -5,12 +5,48 @@ Django admin customizations.
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from core import models
+from django.utils.translation import gettext_lazy as _
 
 
 class UserAdmin(BaseUserAdmin):
     ''' define the admin pages for user'''
     ordering = ['id']
     list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (
+            _('Important dates'),
+            {
+                'fields':('last_login',)
+            }
+        )
+
+    )
+    readonly_fields= ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes':('wide',), # django css concpet for this line.
+            'fields':(
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        }),
+    )
 
 
 admin.site.register(models.User, UserAdmin)
--- app/core/test/test_admin.py
+++ app/core/test/test_admin.py
@@ -31,7 +31,14 @@ class AdminSiteTest(TestCase):
 
     def test_edit_user_page(self):
         ''' Test edit users page'''
-        url = reverse('admin:core_user_changelist', args=[self.user.id])
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        ''' Test create user page'''
+        url = reverse('admin:core_user_add')
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
