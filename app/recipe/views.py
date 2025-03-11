e9c415661ada86c5d60e1c289557c44ea50efb61
naveenprakashjk24 <naveenprakashjk24@gmail.com>
2025-03-11 17:58:54 +0530

    tag test case update

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -34,12 +34,12 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
 class TagViewSet(mixins.UpdateModelMixin, 
                  mixins.ListModelMixin, viewsets.GenericViewSet):
-    '''Manage Tags in the database'''
+    """Manage Tags in the database"""
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        '''Filter queryset to authendicated by user'''
+        """ Filter queryset to authenticated by user """
         return self.queryset.filter(user=self.request.user).order_by('-name')
